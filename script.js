74f6e8600f9aadf8ad686b198def041e63137f0f
rahul shukla <rahul.shukla754@gmail.com>
2021-08-08 15:59:03 +0530

    docs:  use fetch function and call http request

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const  tranButton = document.querySelector('#tranButton');
 function textTranslation(){
     const inputText = document.querySelector('#inputText').value;
     const outputArea =  document.querySelector('#translate-output');
+    
     outputArea.innerHTML = inputText;
 }
+
+
+fetch('https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json')
+.then(res => res.json() )
+.then(data => console.log(data));
 
 
 tranButton.addEventListener('click',textTranslation);
